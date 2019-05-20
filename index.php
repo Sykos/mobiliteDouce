6275d7b87b367d3086baa5108b6e497eaa115fc8
Luca Labate <labatel@protonmail.ch>
2019-05-20 15:27:09 +0200

    -

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Site de l'office du tourisme - Home</title>
+</head>
+<body>
+</header>
+  <div class="images/image_logo">
+    <img src="images/body.jpg" alt="">
+  </div>
+</header>
+<h1>Bienvenue sur le site de l'office du tourisme - Home</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mattis fermentum hendrerit. Integer aliquam dui sed enim auctor faucibus. Praesent aliquet lorem velit, vitae pulvinar risus tincidunt at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus semper nibh elementum vulputate egestas. Maecenas malesuada placerat neque eget faucibus. Duis pellentesque efficitur elit. Etiam non risus mi. Proin ut dui urna. Mauris ut lacus eget arcu porta accumsan at id ligula. Morbi eget sapien ac tortor condimentum vehicula vel in lorem. Etiam elementum turpis augue, sed cursus magna sodales at. Aliquam dictum scelerisque urna, eu maximus diam consequat eu. Praesent posuere elit sed sapien pharetra pulvinar. Nulla nisl lacus, ornare in magna congue, tristique tempus magna. Vestibulum accumsan enim ac tellus lacinia, sit amet laoreet lectus tincidunt. </p>
+
+</body>
+</html>
